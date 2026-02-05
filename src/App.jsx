ee8988f07389e97f0c15ebb97e47daa14847c1f8
byungil712 <jam1598@naver.com>
2026-02-05 10:10:09 +0900

    2026-02-05 두번째 수정

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h2>2026-02-05</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
